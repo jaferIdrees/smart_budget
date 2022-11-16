36f2d08e4d99c9c8f5e818bef9a9094c160fab10
jaferIdrees <jaferll@gmail.com>
2022-11-17 00:19:56 +0300

    Add group card

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
 # inside app/controllers/home_controller.rb
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
 
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :purchases
+
 end
