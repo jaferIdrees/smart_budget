14de28e8de7aab0ce3c137ea1cba5e373fa81bb6
jaferidrees <jaferll@gmail.com>
2022-11-19 13:48:08 +0100

    Add sign in test

---
+++ spec/system/sign_in_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe "User signs in", type: :system do
+  before do
+    @user = create :user
+    visit new_user_session_path
+  end
+
+  scenario "valid with correct credentials" do
+    fill_in "user_email", with: @user.email
+    fill_in "user_password", with: @user.password
+    click_button "Log in"
+
+    expect(page).to have_text "Signed in successfully"
+    
+    expect(page).to have_button "Log out"
+    expect(page).to have_current_path groups_path
+  end
+
+  scenario "invalid with unregistered account" do
+    fill_in "user_email", with: Faker::Internet.email
+    fill_in "user_password", with: "FakePassword123"
+    click_button "sign in"
+
+    expect(page).to have_no_text "Welcome back"
+    expect(page).to have_text "Invalid Email or password."
+    expect(page).to have_no_link "Sign Out"
+  end
+
+  scenario "invalid with invalid password" do
+    fill_in "user_email", with: @user.email
+    fill_in "user_password", with: "FakePassword123"
+    click_button "sign in"
+
+    expect(page).to have_no_text "Welcome back"
+    expect(page).to have_text "Invalid Email or password."
+    expect(page).to have_no_link "Sign Out"
+  end
+end
